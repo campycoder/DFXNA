05167b315c34e3e597e5916e1d8ab3128c5059d8
Ethan Brooks <EB5473@gmail.com>
2015-06-28 20:31:31 -0400

    Commented up CreatureManager

--- DwarfFortressXNA/DwarfFortressXNA/Managers/CreatureManager.cs
+++ DwarfFortressXNA/DwarfFortressXNA/Managers/CreatureManager.cs
@@ -5,12 +5,19 @@ namespace DwarfFortressXNA.Managers
 {
     public class CreatureManager : IObjectManager
     {
+        /// <summary>
+        /// List of creature definitions, indexed by their CREATURE token name.
+        /// </summary>
         public Dictionary<string, Creature> CreatureList;
         public CreatureManager()
         {
             CreatureList = new Dictionary<string, Creature>();
         }
 
+        /// <summary>
+        /// Internal Function. Create a definition and add it to the CreatureList.
+        /// </summary>
+        /// <param name="currentBuffer">Token buffer for the definition.</param>
         public void AddToList(List<string> currentBuffer)
         {
             var name = RawFile.StripTokenEnding(currentBuffer[0].Remove(0, 10));
@@ -18,6 +25,10 @@ namespace DwarfFortressXNA.Managers
             CreatureList.Add(name, creature);
         }
 
+        /// <summary>
+        /// Internal Function. Initiates the appropriate parsing/adding process for the requested token buffer.
+        /// </summary>
+        /// <param name="tokens">Token buffer to parse.</param>
         public void ParseFromTokens(List<string> tokens)
         {
             var currentBuffer = new List<string>();
